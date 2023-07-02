a6249e99deb7fa20e833a71403829c92e8966f37
Alec Lima <aleclimadev@gmail.com>
2023-07-02 07:06:56 -0700

    build: axios and styled-components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+const Navbar = () => {
+  return (
+    <header>
+        <li>testando</li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </header>
+  )
+}
+
+export default Navbar
